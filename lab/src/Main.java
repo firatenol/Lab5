be4b29fb0a60002248d10914e33797bddde4685d
firatenol <e.karuzin@inbox.ru>
2026-05-07 16:52:36 +0300

    Update Main №2

--- lab/src/Main.java
+++ lab/src/Main.java
@@ -2,5 +2,6 @@ class Main{
     public static void main(String[] args) {
         System.out.print("Это добавлено в ветке main");
         System.out.println("Изменение 1");
+        System.out.println("Изменение 2");
     }
 }
