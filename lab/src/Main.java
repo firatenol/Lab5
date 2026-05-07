74b89b9b2544eed63ad0014503f2289f56d9dd8e
firatenol <e.karuzin@inbox.ru>
2026-05-07 17:04:34 +0300

    Update Main in dev #4

--- lab/src/Main.java
+++ lab/src/Main.java
@@ -4,5 +4,6 @@ class Main{
         System.out.println("Hello world!");
         System.out.println("Hello world! #2");
         System.out.println("Hello world! №3");
+        System.out.println("Hello world! №4");
     }
 }
